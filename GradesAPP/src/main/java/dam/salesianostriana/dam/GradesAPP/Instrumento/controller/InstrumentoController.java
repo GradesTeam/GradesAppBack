2a883d66bb8184a87daf949a76e28c3c43f7ead2
Javier Prieto <jpojaviprior@gmail.com>
2023-11-22 22:30:47 +0100

    Finished with no exception waiting for the base security code to finish my work

--- GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/Instrumento/controller/InstrumentoController.java
+++ GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/Instrumento/controller/InstrumentoController.java
@@ -1,15 +1,26 @@
 package dam.salesianostriana.dam.GradesAPP.Instrumento.controller;
 
+import dam.salesianostriana.dam.GradesAPP.Instrumento.GETInstrumentoDTO;
 import dam.salesianostriana.dam.GradesAPP.Instrumento.service.InstrumentoService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/instrumento")
 public class InstrumentoController {
     private final InstrumentoService service;
+
+
+    @GetMapping("/asignatura/{id}/instrumentos")
+    public List<GETInstrumentoDTO> getAllInstrumentosFromAsignatura(@PathVariable UUID id){
+        return service.getAllInstrumentosFromAsignatura(id);
+    }
 }
--- GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/Instrumento/service/InstrumentoService.java
+++ GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/Instrumento/service/InstrumentoService.java
@@ -1,11 +1,24 @@
 package dam.salesianostriana.dam.GradesAPP.Instrumento.service;
 
+import dam.salesianostriana.dam.GradesAPP.Instrumento.GETInstrumentoDTO;
+import dam.salesianostriana.dam.GradesAPP.Instrumento.model.Instrumento;
 import dam.salesianostriana.dam.GradesAPP.Instrumento.repository.InstrumentoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class InstrumentoService {
     private final InstrumentoRepository repo;
+
+    public List<GETInstrumentoDTO> getAllInstrumentosFromAsignatura(UUID id){
+        List<Instrumento> result = repo.getAllInstrumentosByAsignaturaId(id);
+        if(result.isEmpty())
+            throw ;
+        return result.stream().map(GETInstrumentoDTO::of).toList();
+    }
 }
