861fa6a3b0ec9f28e0c68be5d92d444a1620e0ce
A.Jimenez <jimenezalejandro505@gmail.com>
2023-11-24 14:24:04 +0100

    User register en curso

--- GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/user/controller/UserController.java
+++ GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/user/controller/UserController.java
@@ -1,13 +1,25 @@
 package dam.salesianostriana.dam.GradesAPP.user.controller;
 
+import dam.salesianostriana.dam.GradesAPP.user.dto.UserDtoRegister;
+import dam.salesianostriana.dam.GradesAPP.user.model.User;
 import dam.salesianostriana.dam.GradesAPP.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/student")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDtoRegister> save (@RequestBody UserDtoRegister u){
+        userService.saveAlumno(u);
+        return ResponseEntity.status(201).body(u);
+    }
+
 }
--- GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/user/service/UserService.java
+++ GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/user/service/UserService.java
@@ -1,12 +1,16 @@
 package dam.salesianostriana.dam.GradesAPP.user.service;
 
+import dam.salesianostriana.dam.GradesAPP.alumno.model.Alumno;
+import dam.salesianostriana.dam.GradesAPP.user.dto.UserDtoRegister;
 import dam.salesianostriana.dam.GradesAPP.user.model.User;
+import dam.salesianostriana.dam.GradesAPP.user.model.UserRole;
 import dam.salesianostriana.dam.GradesAPP.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -20,5 +24,16 @@ public class UserService {
 
     public Optional<User> findByUserName(String name){
         return repo.findFirstByUsername(name);
+    }
+
+    public Optional<User> saveAlumno (UserDtoRegister u){
+        return Optional.of(repo.save(Alumno.builder()
+                .nombre(u.getNombre())
+                .apellidos(u.getApellidos())
+                .password(u.getPassword())
+                .roles(Set.of(UserRole.USER))
+                .fechaNacimiento(u.getFechaNacimiento())
+                .build()
+        ));
     }
 }
