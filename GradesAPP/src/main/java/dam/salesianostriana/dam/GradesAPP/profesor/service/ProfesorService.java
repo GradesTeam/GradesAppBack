3f6ba5b138417717eab088f6b13843e01064a148
Antonio Martínez <nonomartinezlopez2004@gmail.com>
2023-11-26 18:31:46 +0100

    hu en proceso

--- GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/profesor/service/ProfesorService.java
+++ GradesAPP/src/main/java/dam/salesianostriana/dam/GradesAPP/profesor/service/ProfesorService.java
@@ -3,6 +3,7 @@ package dam.salesianostriana.dam.GradesAPP.profesor.service;
 import dam.salesianostriana.dam.GradesAPP.MyPage;
 import dam.salesianostriana.dam.GradesAPP.alumno.dto.GetAlumnoListDTO;
 import dam.salesianostriana.dam.GradesAPP.alumno.model.Alumno;
+import dam.salesianostriana.dam.GradesAPP.exception.NotFoundException;
 import dam.salesianostriana.dam.GradesAPP.profesor.repository.ProfesorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +21,8 @@ public class ProfesorService {
 
     public MyPage<GetAlumnoListDTO> obtenerAlumnosPorProfesor(UUID profesorId, Pageable pageable){
         Page<Alumno> result = repo.findAlumnosByProfesor(profesorId, pageable);
+        if(result.isEmpty())
+            throw new NotFoundException("Profesor");
         Page<GetAlumnoListDTO> resDto = result.map(GetAlumnoListDTO::of);
         return MyPage.of(resDto);
     }
